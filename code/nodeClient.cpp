5fd2795ae44a092c00f12d7495367bf070b03a97
rahul <rahul-arora@outlook.com>
2021-11-26 05:55:45 -0800

    only testing join for now

--- code/nodeClient.cpp
+++ code/nodeClient.cpp
@@ -5,9 +5,8 @@ using namespace std;
 int main(int argc, char * argv[]){
     Node n0(5); // node_id == 5
     Node n1(63); // node_id == 63
+    Node n2(30); // node_id == 30
     n0.join(NULL); // the first node to join the Chord network.
-    // n0.insert(3, 3); // insert key = 3
     n1.join(&n0); // the second node join the Chord network.
-    // n1.insert(5, 50); // insert key = 5
-    // n0.find(5); // key query
+    n2.join(&n1); // the third node join the Chord network.
 }
